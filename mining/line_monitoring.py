9aa4b435f143b29e5e1c63fc720814c6ba1f10b5
minimaximal <maximilian.koch@hk.mailbox.de>
2023-11-26 17:03:41 +0100

    created mining python scripts

---
+++ mining/line_monitoring.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import datetime
+
+url = "https://graphql-sandbox-dds.rnv-online.de/"
+
+
+
+f = open('/data/rnv_big-data_mining/data/general/auth.json')
+auth = json.load(f)
+f.close()
+
+headers = {
+  'Authorization': auth['token_type'] + ' ' + auth['access_token'],
+  'Content-Type': 'application/json'
+}
+
+# Get the current timestamp
+now = datetime.datetime.now()
+
+cursor = "0"
+while cursor != "999-X" and cursor != "null":
+   payload = "{\"query\":\"{lines (first: 50 after:\\\"" + cursor + "\\\")\\n    {totalCount elements  {\\n        ... on Line\\n        {\\n            id  \\n\\n            journeys { elements {\\n              ... on Journey {\\n                          canceled\\n    \\n          stops    {\\n            \\n            destinationLabel\\n\\n            station {\\n                id\\n            }\\n\\n            plannedDeparture {\\n              isoString\\n            }\\n            \\n            realtimeDeparture {\\n              isoString\\n                }\\n              }\\n            }\\n          }\\n        }\\n      }\\n    }\\n  cursor\\n  }\\n}\",\"variables\":{}}"
+   response = requests.request("POST", url, headers=headers, data=payload)
+   filename = 'line_monitoring_all_{}_'.format(now.strftime('%Y%m%d%H%M%S')) + cursor + '.json'
+   
+   # print(response.text)
+   with open('/data/rnv_big-data_mining/data/mined/' + filename, 'w') as f:
+      f.write(response.text)
+
+   cursor = json.loads(response.text)['data']['lines']['cursor']
---
+++ mining/stations_all.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import datetime
+
+url = "https://graphql-sandbox-dds.rnv-online.de/"
+
+payload = "{\"query\":\"{\\n  stations(first: 5000 lat:49.487758 long:8.466272 distance:99) { totalCount\\n    elements {\\n      ... on Station {\\n          id\\n          hafasID\\n          globalID\\n          name\\n          longName\\n          place\\n          location {\\n            lat\\n            long\\n            hash\\n         }\\n        hasVRNStops  \\n        # journeys\\n        # lines\\n        # platforms\\n        # poles \\n      }\\n    }\\n  }\\n}\",\"variables\":{}}"
+
+f = open('/data/rnv_big-data_mining/data/general/auth.json')
+auth = json.load(f)
+f.close()
+
+
+headers = {
+  'Authorization': auth['token_type'] + ' ' + auth['access_token'],
+  'Content-Type': 'application/json'
+}
+
+# Get the current timestamp
+now = datetime.datetime.now()
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+filename = 'stations_all_{}.json'.format(now.strftime('%Y-%m-%d_%H-%M-%S'))
+
+# print(response.text)
+with open('/data/rnv_big-data_mining/data/general/' + filename, 'w') as f:
+    f.write(response.text)
